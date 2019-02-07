d055797901e16b519bbf12dbafe6478226787786
Fidelis <atosfiel@gmail.com>
2019-02-07 19:08:44 -0200

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portal';
+  texto : string = 'Mapas';
+  lat: number = -23.8779431;
+  lng: number = -49.8046873;
+  zoom: number = 15;
+  elementType:'url'|'canvas'|'img' = 'url';
+  value:string = 'Portal';
+  
+  printDiv = function(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { NgxQRCodeModule } from 'ngx-qrcode2';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	NgxQRCodeModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBm2vF6eXB3bOQ3qknG1VXzJ95C-LcCJvI'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
